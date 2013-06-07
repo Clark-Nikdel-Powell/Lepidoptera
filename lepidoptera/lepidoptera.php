f8fd84f2cffc235175df6319636e151dc84f8330
Josh Nederveld <joshnederveld@gmail.com>
2013-06-07 15:38:42 -0400

    Updated Lepi for Twitter OAuth, yanked out Yelp, because… yeah.

--- lepidoptera/lepidoptera.php
+++ lepidoptera/lepidoptera.php
@@ -23,30 +23,44 @@ GNU General Public License for more details.
 /** ===============================================================================
  *
  * Social Media in Settings
- * 
+ *
 **/
 
 add_action('admin_init', 'social_info');
 function social_info() {
-	
+
+	// General Social Media Settings
 	add_settings_section( 'GENERAL_social', 'Social Media Info', 'GENERAL_social_callback', 'general' );
-	
+
 	add_settings_field( 'facebook_url', 'Facebook URL', 'facebook_callback', 'general', 'GENERAL_social' );
 	add_settings_field( 'gplus_url', 'Google+ URL', 'gplus_callback', 'general', 'GENERAL_social' );
-	add_settings_field( 'twitter_handle', 'Twitter Handle', 'twitter_callback', 'general', 'GENERAL_social' );
 	add_settings_field( 'youtube_url', 'YouTube URL', 'youtube_callback', 'general', 'GENERAL_social' );
 	add_settings_field( 'vimeo_url', 'Vimeo URL', 'vimeo_callback', 'general', 'GENERAL_social' );
 	add_settings_field( 'flickr_url', 'Flickr URL', 'flickr_callback', 'general', 'GENERAL_social' );
 	add_settings_field( 'rss_url', 'RSS URL', 'rss_callback', 'general', 'GENERAL_social' );
-	
+
 	register_setting( 'general', 'facebook_url' );
 	register_setting( 'general', 'gplus_url' );
-	register_setting( 'general', 'twitter_handle' );
 	register_setting( 'general', 'youtube_url' );
 	register_setting( 'general', 'vimeo_url' );
 	register_setting( 'general', 'flickr_url' );
 	register_setting( 'general', 'rss_url' );
-	
+
+	// Twitter Settings
+	add_settings_section( 'GENERAL_twitter', 'Twitter Settings', 'GENERAL_twitter_callback', 'general' );
+
+	add_settings_field( 'twitter_handle',  		   'Twitter Handle', 		'twitter_handle_callback', 			'general', 'GENERAL_twitter' );
+	add_settings_field( 'tw_consumer_key', 			'Consumer Key', 			'tw_consumer_key_callback', 			'general', 'GENERAL_twitter' );
+	add_settings_field( 'tw_consumer_secret', 		'Consumer Secret', 		'tw_consumer_secret_callback', 		'general', 'GENERAL_twitter' );
+	add_settings_field( 'tw_access_token', 			'Access Token', 			'tw_access_token_callback', 			'general', 'GENERAL_twitter' );
+	add_settings_field( 'tw_access_token_secret', 	'Access Token Secret', 	'tw_access_token_secret_callback', 	'general', 'GENERAL_twitter' );
+
+	register_setting( 'general', 'twitter_handle' );
+	register_setting( 'general', 'tw_consumer_key' );
+	register_setting( 'general', 'tw_consumer_secret' );
+	register_setting( 'general', 'tw_access_token' );
+	register_setting( 'general', 'tw_access_token_secret' );
+
 } // social_info()
 
 function GENERAL_social_callback() { echo ''; }
@@ -56,9 +70,6 @@ function facebook_callback($args) {
 }
 function gplus_callback($args) {
 	echo '<input type="text" id="gplus_url" name="gplus_url" value="'.get_option('gplus_url').'" style="width:70%" />';
-}
-function twitter_callback($args) {
-	echo '@<input type="text" id="twitter_handle" name="twitter_handle" value="'.get_option('twitter_handle').'" style="width:70%" />';
 }
 function youtube_callback($args) {
 	echo '<input type="text" id="youtube_url" name="youtube_url" value="'.get_option('youtube_url').'" style="width:70%" />';
@@ -73,10 +84,29 @@ function rss_callback($args) {
 	echo '<input type="text" id="rss_url" name="rss_url" value="'.get_option('rss_url').'" style="width:70%" /><p class="description">Default &rarr; '.get_bloginfo('rss2_url').'</p>';
 }
 
+// Twitter Settings
+function GENERAL_twitter_callback() { echo ''; }
+
+function twitter_handle_callback($args) {
+	echo '@<input type="text" id="twitter_handle" name="twitter_handle" value="'.get_option('twitter_handle').'" style="width:70%" />';
+}
+function tw_consumer_key_callback($args) {
+	echo '<input type="text" id="tw_consumer_key" name="tw_consumer_key" value="'.get_option('tw_consumer_key').'" style="width:70%" />';
+}
+function tw_consumer_secret_callback($args) {
+	echo '<input type="text" id="tw_consumer_secret" name="tw_consumer_secret" value="'.get_option('tw_consumer_secret').'" style="width:70%" />';
+}
+function tw_access_token_callback($args) {
+	echo '<input type="text" id="tw_access_token" name="tw_access_token" value="'.get_option('tw_access_token').'" style="width:70%" />';
+}
+function tw_access_token_secret_callback($args) {
+	echo '<input type="text" id="tw_access_token_secret" name="tw_access_token_secret" value="'.get_option('tw_access_token_secret').'" style="width:70%" />';
+}
+
 /** ===============================================================================
  *
  * Facebook Like Button
- * 
+ *
  * Arguments:
  *   'href'        : URL to like
  *   'send'        : 'true', 'false' : Send button
@@ -86,7 +116,7 @@ function rss_callback($args) {
  *   'font'        : 'arial', 'lucida grande', 'segoe ui', 'tahoma', 'trebuchet ms', 'verdana'
  *   'colorscheme' : 'light', 'dark'
  *   'action'      : 'like', 'recommend'
- * 
+ *
 **/
 
 // pull in facebook javascript sdk only once
@@ -94,7 +124,7 @@ function FB_SDK() { include_once('fb-sdk.php'); }
 
 // returns button
 function LEPI_get_fb_button($args=0) {
-	
+
 	$defaults = array(
 		'href'        => get_option('facebook_url')
 	,	'send'        => 'false'
@@ -106,7 +136,7 @@ function LEPI_get_fb_button($args=0) {
 	,	'action'      => 'like'
 	);
 	$vars = wp_parse_args($args, $defaults);
-	
+
 	// build button div
 	$button = '<div class="fb-like" data-href="'.urlencode($vars['href']).'" data-send="'.$vars['send'].'" data-width="'.$vars['width'].'" data-show-faces="'.$vars['show-faces'].'"';
 	if ($vars['layout'] != 'standard') { $button .= ' data-layout="'.$vars['layout'].'"'; }
@@ -114,28 +144,28 @@ function LEPI_get_fb_button($args=0) {
 	if ($vars['colorscheme'] != $defaults['colorscheme']) { $button .= ' data-colorscheme="'.$vars['colorscheme'].'"'; }
 	if ($vars['action'] != $defaults['action']) { $button .= ' data-action="'.$vars['action'].'"'; }
 	$button .= '></div>'.PHP_EOL;
-	
+
 	// put sdk in the footer, return button
 	add_action('wp_footer', 'FB_SDK');
 	return $button;
-	
+
 } // LEPI_get_fb_button()
 
 // displays button
 function LEPI_fb_button($args=0) {
-	
+
 	echo LEPI_get_fb_button($args);
-	
+
 } // LEPI_fb_button()
 
 /** ===============================================================================
  *
  * Google +1 Button
- * 
+ *
  * Arguments:
  *   'href'        : URL to like
  *   'size'        : 'small', 'medium', 'standard', 'tall'
- * 
+ *
 **/
 
 // pull in google javascript sdk only once
@@ -143,47 +173,47 @@ function GP_SDK() { include_once('gp-sdk.php'); }
 
 // returns button
 function LEPI_get_gp_button($args=0) {
-	
+
 	$defaults = array(
 		'href'        => get_option('gplus_url')
 	,	'size'        => 'medium'
 	);
 	$vars = wp_parse_args($args, $defaults);
-	
+
 	// build button div
 	$button = '<g:plusone size="'. $vars[ 'size' ] .'"></g:plusone>';
-	
+
 	// put sdk in the footer, return button
 	add_action('wp_footer', 'GP_SDK');
 	return $button;
-	
+
 } // LEPI_get_fb_button()
 
 // displays button
 function LEPI_gp_button($args=0) {
-	
+
 	echo LEPI_get_gp_button($args);
-	
+
 } // LEPI_gp_button()
 
 
 /** ===============================================================================
  *
  * Twitter Share Button
- * 
+ *
  * Arguments:
  *   'text'     : text of tweet
  *   'url'      : URL to share
  *   'hashtags' : list of hashtags separated by commas
  *   'via'      : reference a twitter handle
  *   'count'    : 'none', 'horizontal', 'vertical'
- * 
+ *
 **/
 
 // returns button
 function LEPI_get_tw_button($args=0) {
 	global $post;
-	
+
 	$defaults = array(
 		'text'     => get_the_title($post->ID)
 	,	'url'      => get_permalink($post->ID)
@@ -192,7 +222,7 @@ function LEPI_get_tw_button($args=0) {
 	,	'count'    => 'horizontal'
 	);
 	$vars = wp_parse_args($args, $defaults);
-	
+
 	// build button div
 	$button = '<a href="https://twitter.com/share" class="twitter-share-button" data-text="'.$vars['text'].'"';
 	if ($vars['url']) { $button .= ' data-url="'.$vars['url'].'"'; }
@@ -201,35 +231,35 @@ function LEPI_get_tw_button($args=0) {
 	if ($vars['count'] != $defaults['count']) { $button .= ' data-count="'.$vars['count'].'"'; }
 	$button .= '>Tweet</a>';
 	$button .= '<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="//platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>'.PHP_EOL;
-	
+
 	// return button
 	return $button;
-	
+
 } // LEPI_get_tw_button()
 
 // displays button
 function LEPI_tw_button($args=0) {
-	
+
 	echo LEPI_get_tw_button($args);
-	
+
 } // LEPI_tw_button()
 
 /** ===============================================================================
  *
  * Pinterest Pin Button
- * 
+ *
  * Arguments:
  *   'url'         : URL of pin
  *   'media'       : URL to image to pin
  *   'description' : Text attached to pin
  *   'pin-config'  : 'beside', 'above', 'none'
- * 
+ *
 **/
 
 // returns button
 function LEPI_get_pin_button($args=0) {
 	global $post;
-	
+
 	$defaults = array(
 		'url'         => get_permalink($post->ID)
 	,	'media'       => wp_get_attachment_url(get_post_thumbnail_id($post->ID))
@@ -237,138 +267,88 @@ function LEPI_get_pin_button($args=0) {
 	,	'pin-config'  => 'beside'
 	);
 	$vars = wp_parse_args($args, $defaults);
-	
+
 	// build button div
 	$button = '<a href="//pinterest.com/pin/create/button/?url='.urlencode($vars['url']).'&media='.$vars['media'].'&description='.$vars['description'].'" data-pin-config="'.$vars['pin-config'].'" data-pin-do="buttonPin" ><img src="//assets.pinterest.com/images/pidgets/pin_it_button.png" /></a>'.PHP_EOL;
-	
+
 	// put js in the footer, return button
 	wp_enqueue_script('pinterest', '//assets.pinterest.com/js/pinit.js', array(), false, true);
 	return $button;
-	
+
 } // LEPI_get_pin_button()
 
 // displays button
 function LEPI_pin_button($args=0) {
-	
+
 	echo LEPI_get_pin_button($args);
-	
+
 } // LEPI_pin_button()
 
 /** ===============================================================================
  *
- * Get tweets with Twitter's search API
- * 
- * @param int    $tweet_count    Number of tweets to return
- * @param string $twitter_handle Twitter handle.. duh
- * 
+ * Get tweets with Twitter's REST API, now with more OAuth!
+ *
+ * @param int    $max_tweets    Number of tweets to return
+ * @param string $twitter_id Twitter handle.. duh
+ *
 **/
 
-function LEPI_get_tweets($tweet_count, $twitter_handle) {
-	
+function LEPI_get_tweets($max_tweets, $twitter_id) {
+
+	require_once 'lib/twitter/tmhOAuth.php';
+
+	$tmhOAuth = new tmhOAuth(array(
+	'consumer_key'    => get_option('tw_consumer_key'),
+	'consumer_secret' => get_option('tw_consumer_secret'),
+	'user_token'      => get_option('tw_access_token'),
+	'user_secret'     => get_option('tw_access_token_secret')
+	));
+
+	$code    = $tmhOAuth->request('GET', $tmhOAuth->url('1/statuses/user_timeline'), array(
+	  'screen_name' => $twitter_id,
+	  'count' => $max_tweets,
+	  'include_rts' => true,
+	  'include_entities' => true
+	));
+
 	$transient_name = $twitter_handle.'_twitter_search_results';
-	
-	$tweets = get_transient($transient_name);
-	
-	if (!$tweets) {
-			
-		$date_limit = date('Y-m-d', strtotime('10 days ago'));
-		$tweets_search = file_get_contents('http://search.twitter.com/search.json?q=from:'.$twitter_handle.'%20since:'.$date_limit.'%20exclude:retweets%20exclude:replies&rpp=10&result_type=recent');
-		$tweets_search = json_decode($tweets_search);
-		
-		$tweets = array();
-		foreach ($tweets_search->results as $tweet) {
-			$tweet_text = $tweet->text;
-	
-			// Add hyperlink html tags to any urls, twitter ids or hashtags in the tweet.
-			$tweet_text = preg_replace('/(\.\.\.+)/', '…', $tweet_text);
-			$tweet_text = preg_replace('/(https?:\/\/[^\s"<>…]+)/','<a href="$1">$1</a>', $tweet_text);
-			$tweet_text = preg_replace('/(^|[\n\s])@([^\s"\t\n\r<:]*)/is', '$1<a href="http://twitter.com/$2">@$2</a>', $tweet_text);
-			$tweet_text = preg_replace('/(^|[\n\s])#([^\s"\t\n\r<:]*)/is', '$1<a href="http://twitter.com/search?q=%23$2">#$2</a>', $tweet_text);
-			
-			// Time zone offsets
-			$estTimezone = new DateTimeZone('America/New_York');
-			$gmtTimezone = new DateTimeZone('GMT');
-			$timestamp   = new DateTime($tweet->created_at, $gmtTimezone);
-			$offset 		 = $estTimezone->getOffset($timestamp);
-			$processed_time = $timestamp->format('U') + $offset;
-			
-			$tweets[] = array(
-				'text'      => $tweet_text
-			,	'timestamp' => $processed_time
-			);
-		}
-		
-		set_transient($transient_name, $tweets, 1 * HOUR_IN_SECONDS);
-		
+
+//	delete_transient($transient_name);
+	$tweets_raw = get_transient($transient_name);
+
+	if ($tweets_raw === false) { //if there is no cached file
+	  $tweets_raw = json_decode($tmhOAuth->response['response'], true);
+	  set_transient($transient_name, $tweets_raw, 1 * HOUR_IN_SECONDS);
+	  $set_tweets = true;
 	}
-	return array_slice($tweets,	0, $tweet_count);
+	if (isset($set_tweets)) { //parse the feed just once as it will be cached from now on
+	  $tweets_raw = json_decode($tmhOAuth->response['response'], true);
+	}
+
+	foreach ($tweets_raw as $tweet) {
+		$tweet_text = $tweet['text'];
+
+		// Add hyperlink html tags to any urls, twitter ids or hashtags in the tweet.
+		$tweet_text = preg_replace('/(\.\.\.+)/', '…', $tweet_text);
+		$tweet_text = preg_replace('/(https?:\/\/[^\s"<>…]+)/','<a href="$1">$1</a>', $tweet_text);
+		$tweet_text = preg_replace('/(^|[\n\s])@([^\s"\t\n\r<:]*)/is', '$1<a href="http://twitter.com/$2">@$2</a>', $tweet_text);
+		$tweet_text = preg_replace('/(^|[\n\s])#([^\s"\t\n\r<:]*)/is', '$1<a href="http://twitter.com/search?q=%23$2">#$2</a>', $tweet_text);
+
+		// Time zone offsets
+		$estTimezone = new DateTimeZone('America/New_York');
+		$gmtTimezone = new DateTimeZone('GMT');
+		$timestamp   = new DateTime($tweet['created_at'], $gmtTimezone);
+		$offset 		 = $estTimezone->getOffset($timestamp);
+		$processed_time = $timestamp->format('U') + $offset;
+
+		$tweets[] = array(
+			'text'      => $tweet_text
+		,	'timestamp' => $processed_time
+		);
+	}
+
+	return array_slice($tweets, 0, $tweet_count);
 
 } // LEPI_get_tweets()
 
-/** ===============================================================================
- *
- * Get reviews with Yelp's business API
- * 
- * @param int    $yelp_count  Number of yelps to return
- * @param string $yelp_slug	
- * 
-**/
-
-function LEPI_get_yelps($args=0) {
-
-$defaults = array(
-	'biz_slug'    	=> 'wruck-paupore-pc-dyer'
-,	'con_key'    	=> 'UiL6-QkZdAbSm9-5U624vw'
-,	'con_secret'	=> 'bWhWrY-TObvrlyfjwq03rJ6J-j0'
-,	'token'			=> 'ZVqfGxA-ENqM5191YljSqRChp8HrQF9Q'
-,	'token_secret'	=> 'yYbGafCHOEy5pKDX6pwGgokdZL0'
-);
-$vars = wp_parse_args($args, $defaults);
-
-$biz_slug = $vars['biz_slug'];
-
-// Enter the path that the oauth library is in relation to the php file
-require_once ('lib/OAuth.php');
-
-$unsigned_url = 'http://api.yelp.com/v2/business/'.$biz_slug;
-
-// Set your keys here
-$consumer_key 		= $vars['con_key'];
-$consumer_secret 	= $vars['con_secret'];
-$token 				= $vars['token'];
-$token_secret 		= $vars['token_secret'];
-
-// Token object built using the OAuth library
-$token = new OAuthToken($token, $token_secret);
-
-// Consumer object built using the OAuth library
-$consumer = new OAuthConsumer($consumer_key, $consumer_secret);
-
-// Yelp uses HMAC SHA1 encoding
-$signature_method = new OAuthSignatureMethod_HMAC_SHA1();
-
-// Build OAuth Request using the OAuth PHP library. Uses the consumer and token object created above.
-$oauthrequest = OAuthRequest::from_consumer_and_token($consumer, $token, 'GET', $unsigned_url);
-
-// Sign the request
-$oauthrequest->sign_request($signature_method, $consumer, $token);
-
-// Get the signed URL
-$signed_url = $oauthrequest->to_url();
-
-// Send Yelp API Call
-$ch = curl_init($signed_url);
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-curl_setopt($ch, CURLOPT_HEADER, 0);
-$data = curl_exec($ch); // Yelp response
-curl_close($ch);
-
-// Handle Yelp response data
-$response = json_decode($data);
-
-$arr = (array) $response;
-
-// Return the data!
-return $arr;
-
-}
+?>
